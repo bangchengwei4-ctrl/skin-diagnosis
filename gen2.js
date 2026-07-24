d86688a25c90b7f762e628bc5bde894ed4b60dc4
bangchengwei4-ctrl <bangchengwei4@gmail.com>
2026-07-24 20:29:58 +0800

    cleanup: remove old file gen2.js

--- gen2.js
+++
@@ -1,3 +0,0 @@
-﻿const fs=require('fs'),path=require('path');const p=path.join(__dirname,'index.html');let h='';
-h+='<!DOCTYPE html><html lang=zh-CN><head><meta charset=UTF-8><title>Test</title></head><body><h1>Test OK</h1></body></html>');
-fs.writeFileSync(p,h,'utf8');console.log('OK: '+h.length);
